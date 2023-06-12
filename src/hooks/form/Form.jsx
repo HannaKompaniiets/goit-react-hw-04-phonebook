9a230191294da5a30448c515f7428bee6d49919f
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-06-12 16:21:40 +0200

    Update Form.jsx

--- src/hooks/form/Form.jsx
+++ src/hooks/form/Form.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"; 
+import { useState } from "react"; 
 import css from './form.module.css';
 import PropTypes from 'prop-types';
 
@@ -41,7 +41,7 @@ const Form = ({ onSubmit }) => {
           onChange={handleInputChange}
           type="text"
           name="name"
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -56,7 +56,7 @@ const Form = ({ onSubmit }) => {
           onChange={handleInputChange}
           type="tel"
           name="number"
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
